f32b4a3df2ffaf1b0189474bb971a4effa32ce49
beratakk61 <beratak@kbyazilim.com.tr>
2022-03-14 01:18:39 +0300

    Add project files.

---
+++ Core/DataAccess/EntityFrameworkCore/EfCoreEntityRepository.cs
@@ -0,0 +1,79 @@
+﻿using Core.Entities.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.EntityFrameworkCore
+{
+    public class EfCoreEntityRepository<TEntity> : IEntityRepository<TEntity>
+        where TEntity : EntityBase, new()
+    {
+        private readonly DbContext _context;
+
+        public EfCoreEntityRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public int Create(TEntity entity)
+        {
+            _context.Set<TEntity>().Add(entity);
+            _context.SaveChanges();
+            return entity.Id;
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter, params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            IQueryable<TEntity> query = _context.Set<TEntity>();
+
+            query = query.Where(filter);
+
+            if (includeProperties.Any())
+            {
+                foreach (var includeProperty in includeProperties)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            return query.FirstOrDefault();
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null, params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            IQueryable<TEntity> query = _context.Set<TEntity>();
+
+            if (filter!=null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProperties.Any())
+            {
+                foreach (var includeProperty in includeProperties)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            return query.ToList();
+        }
+
+        public void Update(TEntity entity)
+        {
+            var updatedEntity = _context.Entry(entity);
+            updatedEntity.State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+    }
+}
