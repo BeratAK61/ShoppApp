169a856786105883ea0291c222083aa9785b92e0
beratakk61 <brt_ak_61@hotmail.com>
2022-03-25 01:05:14 +0300

    Add DTOs For Address

---
+++ ShopApp.Entities/DTOs/AddressAddDto.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Entities.DTOs
+{
+    public class AddressAddDto :  IDto
+    {
+        public string AddressTitle { get; set; }
+        public string District { get; set; }
+        public string Street { get; set; }
+        public string ApartmentNumber { get; set; }
+        public string ApartmentName { get; set; }
+        public string AddressDescription { get; set; }
+
+
+        public string UserId { get; set; }
+    }
+}
---
+++ ShopApp.Entities/DTOs/AddressDto.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities.Abstract;
+using ShopApp.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Entities.DTOs
+{
+    public class AddressDto : DtoBase , IDto
+    {
+        public string AddressTitle { get; set; }
+        public string District { get; set; }
+        public string Street { get; set; }
+        public string ApartmentNumber { get; set; }
+        public string ApartmentName { get; set; }
+        public string AddressDescription { get; set; }
+
+
+        public string UserId { get; set; }
+    }
+}
