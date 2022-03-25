a6232cdd288751f84ef006a2ab3ac583770268f7
beratakk61 <brt_ak_61@hotmail.com>
2022-03-26 02:36:39 +0300

    Add DTOs For Product

---
+++ ShopApp.Entities/DTOs/ProductDto.cs
@@ -0,0 +1,31 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Entities.DTOs
+{
+    public class ProductDto : DtoBase , IDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+        public int Star { get; set; }
+        public string Unit { get; set; }
+        public decimal Quantity { get; set; }
+        public string Barcode { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsFeatured { get; set; }
+        public bool IsHome { get; set; }
+        public bool IsSlider { get; set; }
+
+
+        public int CategoryId { get; set; }
+
+
+        public int BrandId { get; set; }
+    }
+}
---
+++ ShopApp.Entities/DTOs/ProductUpdateDto.cs
@@ -0,0 +1,34 @@
+﻿using Core.Entities.Abstract;
+using ShopApp.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Entities.DTOs
+{
+    public class ProductUpdateDto : DtoBase , IDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+        public int Star { get; set; }
+        public string Unit { get; set; }
+        public decimal Quantity { get; set; }
+        public string Barcode { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsFeatured { get; set; }
+        public bool IsHome { get; set; }
+        public bool IsSlider { get; set; }
+
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+
+        public int BrandId { get; set; }
+        public Brand Brand { get; set; }
+    }
+}
