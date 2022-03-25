fb66b125d217ed9914346b139b12738cfb43130f
beratakk61 <brt_ak_61@hotmail.com>
2022-03-26 01:58:18 +0300

    Add DTOs For Brand

---
+++ ShopApp.Entities/DTOs/BrandUpdateDto.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Entities.DTOs
+{
+    public class BrandUpdateDto : DtoBase , IDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
