e135a13bcf9a2aa9d946b1a43cd8ed031167afc4
beratakk61 <brt_ak_61@hotmail.com>
2022-03-26 02:03:09 +0300

    Add DTOs For Cart

---
+++ ShopApp.Entities/DTOs/CartUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities.Abstract;
+using ShopApp.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Entities.DTOs
+{
+    public class CartUpdateDto : DtoBase , IDto
+    {
+        public string UserId { get; set; }
+        public User User { get; set; }
+    }
+}
