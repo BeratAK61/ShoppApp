37e257b920feb733d285f527b2bcdca7beded663
beratakk61 <brt_ak_61@hotmail.com>
2022-03-26 13:31:21 +0300

    Add CategoryProfile

---
+++ ShopApp.Business/Mapping/Profiles/CategoryProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using ShopApp.Entities.Concrete;
+using ShopApp.Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Business.Mapping.Profiles
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<CategoryAddDto, Category>()
+                .ForMember(dest => dest.AddedTime, opt => opt.MapFrom(x => DateTime.Now))
+                .ForMember(dest => dest.ModifiedTime, opt => opt.MapFrom(x => DateTime.Now))
+                .ForMember(dest => dest.IsActive, opt => opt.MapFrom(x => true));
+
+            CreateMap<CategoryUpdateDto, Category>()
+                .ForMember(dest => dest.ModifiedTime, opt => opt.MapFrom(x => DateTime.Now));
+
+            CreateMap<CategoryDto, Category>().ReverseMap();
+
+        }
+    }
+}
