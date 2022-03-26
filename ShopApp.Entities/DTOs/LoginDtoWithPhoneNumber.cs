1086405405646d47f9f35959429e8379cd71cf90
beratakk61 <brt_ak_61@hotmail.com>
2022-03-26 13:53:56 +0300

    Add RegisterDto and LoginDtos

---
+++ ShopApp.Entities/DTOs/LoginDtoWithPhoneNumber.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Entities.DTOs
+{
+    public class LoginDtoWithPhoneNumber : IDto
+    {
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ ShopApp.Entities/DTOs/RegisterDto.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Entities.DTOs
+{
+    public class RegisterDto : IDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Age { get; set; }
+    }
+}
