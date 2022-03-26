4ef8d06c4f02324bb015d88e9e19d905ac72e391
beratakk61 <brt_ak_61@hotmail.com>
2022-03-26 03:01:53 +0300

    Add AutoMapper And AddressProfile

---
+++ ShopApp.Business/Mapping/Profiles/AddressProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using ShopApp.Entities.Concrete;
+using ShopApp.Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Business.Mapping.Profiles
+{
+    public class AddressProfile : Profile
+    {
+        public AddressProfile()
+        {
+            CreateMap<AddressAddDto, Address>()
+                .ForMember(dest => dest.AddedTime, opt => opt.MapFrom(x => DateTime.Now))
+                .ForMember(dest => dest.ModifiedTime, opt => opt.MapFrom(x => DateTime.Now))
+                .ForMember(dest => dest.IsActive, opt => opt.MapFrom(x => true));
+        }
+    }
+}
