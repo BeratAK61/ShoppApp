53181a22a37e82d5feab10a7868ad7fbf000ea96
beratakk61 <brt_ak_61@hotmail.com>
2022-03-26 14:12:28 +0300

    Add IAddressService To Business

---
+++ ShopApp.Business/Abstract/IAddressService.cs
@@ -0,0 +1,19 @@
+﻿using Core.Utilities.Results.Abstract;
+using ShopApp.Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Business.Abstract
+{
+    public interface IAddressService
+    {
+        IDataResult<List<AddressDto>> GetAll();
+        IDataResult<AddressUpdateDto> GetById(int? id);
+        IDataResult<int> Create(AddressAddDto addressAddDto);
+        IResult Update(AddressUpdateDto addressUpdateDto);
+        IResult Delete(int? id);
+    }
+}
