4bbf3091aa95c0d83afcfcaea02c3e9f169a6b3f
beratakk61 <brt_ak_61@hotmail.com>
2022-03-26 14:49:40 +0300

    Add AddressManager,Messages Class and Fix Bug On DataResult.cs

---
+++ ShopApp.Business/Concrete/AddressManager.cs
@@ -0,0 +1,91 @@
+﻿using AutoMapper;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using ShopApp.Business.Abstract;
+using ShopApp.Business.Utilities;
+using ShopApp.DataAccess.Abstract;
+using ShopApp.Entities.Concrete;
+using ShopApp.Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Business.Concrete
+{
+    public class AddressManager : IAddressService
+    {
+        private readonly IAddressDal _addressDal;
+        private readonly IMapper _mapper;
+
+        public AddressManager(IAddressDal addressDal, IMapper mapper)
+        {
+            _addressDal = addressDal;
+            _mapper = mapper;
+        }
+
+        public IDataResult<int> Create(AddressAddDto addressAddDto)
+        {
+            if (addressAddDto != null)
+            {
+                return new SuccessDataResult<int>(_addressDal.Create(_mapper.Map<Address>(addressAddDto)), Messages.AddingCompleted);
+            }
+            return new ErrorDataResult<int>(Messages.AddingCompleted);
+
+        }
+
+        public IResult Delete(int? id)
+        {
+            if (id != null)
+            {
+                var entity = _addressDal.Get(a => a.Id == (int)id);
+                if (entity != null)
+                {
+                    _addressDal.Delete(entity);
+                    return new SuccessResult(Messages.DeletingCompleted);
+                }
+                return new ErrorResult(Messages.DeletingNotCompleted);
+            }
+            return new ErrorResult(Messages.DeletingNotCompleted);
+        }
+
+        public IDataResult<List<AddressDto>> GetAll()
+        {
+            var entities = _addressDal.GetAll();
+            if (entities != null)
+            {
+                if (entities.Count > 0)
+                {
+                    return new SuccessDataResult<List<AddressDto>>(_mapper.Map<List<AddressDto>>(entities), Messages.ListingCompleted);
+                }
+                return new ErrorDataResult<List<AddressDto>>(Messages.ThereIsNoDataInTable);
+            }
+            return new ErrorDataResult<List<AddressDto>>(Messages.ListingNotCompleted);
+        }
+
+        public IDataResult<AddressUpdateDto> GetById(int? id)
+        {
+            if (id != null)
+            {
+                var entity = _addressDal.Get(a => a.Id == (int)id);
+                if (entity != null)
+                {
+                    return new SuccessDataResult<AddressUpdateDto>(_mapper.Map<AddressUpdateDto>(entity), Messages.DeletingCompleted);
+                }
+                return new ErrorDataResult<AddressUpdateDto>(Messages.GettingNotCompleted);
+            }
+            return new ErrorDataResult<AddressUpdateDto>(Messages.GettingNotCompleted);
+        }
+
+        public IResult Update(AddressUpdateDto addressUpdateDto)
+        {
+            if (addressUpdateDto != null)
+            {
+                _addressDal.Update(_mapper.Map<Address>(addressUpdateDto));
+                return new SuccessResult(Messages.UpdatingCompleted);
+            }
+            return new ErrorResult(Messages.UpdatingNotCompleted);
+        }
+    }
+}
---
+++ ShopApp.Business/Utilities/Messages.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Business.Utilities
+{
+    public static class Messages
+    {
+        public static string AddingCompleted = "Ekleme Başarılı";
+        public static string UpdatingCompleted = "Güncelleme Başarılı";
+        public static string DeletingCompleted = "Silme Başarılı";
+        public static string ListingCompleted = "Listeleme Başarılı";
+        public static string GettingCompleted = "Görüntüleme Başarılı";
+
+        public static string ThereIsNoDataInTable = "Tabloda Hiç Veri Yok!";
+
+        public static string AddingNotCompleted = "Ekleme Başarısız!";
+        public static string UpdatingNotCompleted = "Güncelleme Başarısız!";
+        public static string DeletingNotCompleted = "Silme Başarısız!";
+        public static string ListingNotCompleted = "Listeleme Başarısız!";
+        public static string GettingNotCompleted = "Görüntüleme Başarısız!";
+
+        public static string CreatingTokenCompleted = "Token Oluşturma Başarılı";
+        public static string CreatingTokenNotCompleted = "Token Oluşturma Başarısız!";
+
+        public static string LoginError = "E-Posta veya Şifre yanlış";  //Tek bir hata donuyoruz guvenlik amacli
+
+        public static string UserNotFound = "Kullancı Bulunamadı";
+
+        public static string TokenFound = "Token Bulundu";
+        public static string TokenNotFound = "Token Bulunamadi";
+
+        public static string LoginSuccess = "Giriş Başarılı";
+
+        public static string TokenDeletingCompleted = "Token Silindi";
+
+        public static string ClientNotFound = "Client Bulunamadi";
+        public static string UserNotAdded = "Kullanıcı Oluşturulamadı";
+        public static string UserAdded = "Kullanıcı Oluşturuldu";
+
+        public static string UserChecked = "Kullanıcı Doğrulandı";
+        public static string UserNotChecked = "Kullanıcı Bulunamadı";
+
+        public static string EmailSent = "E-Posta Gönderildi";
+
+
+
+        public static string InvalidDateOfProduction = "Lütfen geçerli bir tarih giriniz";
+        public static string InvalidInputTypes = "Lütfen doğru formlarda veri giriniz";
+
+        public static string UserVerified = "Kulanıcı Doğrulandı";
+        public static string UserNotVerified = "Kullanıcı Doğrulanamadı,Lütfen Ad,Soyad,T.C. Kimlik No ve Doğum Tarihi alanlarını doğru giriniz";
+
+        public static string NotUniqueUsername = "Bu Kullanıcı Adı Daha Önce Alınmış,Lütfen Başka Bir Kullanıcı Adı Seçiniz";
+        public static string UserConfirmed = "Kullanıcı Onaylandı";
+        public static string UserNotConfirmed = "Kullanıcı Onaylanmadı";
+
+        public static string UndoDeletingCompleted = "Silinen Veri Geri Getirildi";
+        public static string UndoDeletingNotCompleted = "Silinen Veri Geri Getirilemedi";
+    }
+}
