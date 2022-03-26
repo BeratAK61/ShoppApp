7449478ca91ab249bb41b23f49cfb7575ffe13eb
beratakk61 <brt_ak_61@hotmail.com>
2022-03-26 13:38:32 +0300

    Add ProductProfile

---
+++ ShopApp.Business/Mapping/Profiles/ProductProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using ShopApp.Entities.Concrete;
+using ShopApp.Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Business.Mapping.Profiles
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<ProductAddDto, Product>()
+                .ForMember(dest => dest.AddedTime, opt => opt.MapFrom(x => DateTime.Now))
+                .ForMember(dest => dest.ModifiedTime, opt => opt.MapFrom(x => DateTime.Now))
+                .ForMember(dest=>dest.IsActive,opt=>opt.MapFrom(x=>true))
+                .ForMember(dest => dest.IsActive, opt => opt.MapFrom(x => true));
+
+            CreateMap<ProductUpdateDto, Product>()
+                .ForMember(dest => dest.ModifiedTime, opt => opt.MapFrom(x => DateTime.Now));
+
+            CreateMap<ProductDto, Product>().ReverseMap();
+
+        }
+    }
+}
