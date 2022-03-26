1f857f8f368db6700e24d49730dd12cf1b6294bc
beratakk61 <brt_ak_61@hotmail.com>
2022-03-26 13:24:19 +0300

    Add CartItemProfile

---
+++ ShopApp.Business/Mapping/Profiles/CartItemProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using ShopApp.Entities.Concrete;
+using ShopApp.Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Business.Mapping.Profiles
+{
+    public class CartItemProfile : Profile
+    {
+        public CartItemProfile()
+        {
+            CreateMap<CartItemAddDto, CartItem>()
+                .ForMember(dest => dest.AddedTime, opt => opt.MapFrom(x => DateTime.Now))
+                .ForMember(dest => dest.ModifiedTime, opt => opt.MapFrom(x => DateTime.Now))
+                .ForMember(dest => dest.IsActive, opt => opt.MapFrom(x=>true));
+
+            CreateMap<CartItemUpdateDto, CartItem>()
+                .ForMember(dest => dest.ModifiedTime, opt => opt.MapFrom(x => DateTime.Now));
+
+            CreateMap<CartItemDto, CartItem>().ReverseMap();
+
+        }
+    }
+}
