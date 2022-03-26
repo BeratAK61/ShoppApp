bc20dee9dbb55e4b944a03514e951c2501162703
beratakk61 <brt_ak_61@hotmail.com>
2022-03-26 13:33:13 +0300

    Add OrderItem Profile

---
+++ ShopApp.Business/Mapping/Profiles/OrderItemProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using ShopApp.Entities.Concrete;
+using ShopApp.Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Business.Mapping.Profiles
+{
+    public class OrderItemProfile : Profile
+    {
+        public OrderItemProfile()
+        {
+            CreateMap<OrderItemAddDto, OrderItem>()
+                .ForMember(dest => dest.AddedTime, opt => opt.MapFrom(x => DateTime.Now))
+                .ForMember(dest => dest.ModifiedTime, opt => opt.MapFrom(x => DateTime.Now))
+                .ForMember(dest => dest.IsActive, opt => opt.MapFrom(x => true));
+
+            CreateMap<OrderItemUpdateDto, OrderItem>()
+                .ForMember(dest => dest.ModifiedTime, opt => opt.MapFrom(x => DateTime.Now));
+
+            CreateMap<OrderItemDto, OrderItem>().ReverseMap();
+        }
+    }
+}
